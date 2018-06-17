e38846d4b06f738f0d0893f0b138e099f0bc8236
Takuya Yoshikane <tak719@gmail.com>
2018-06-17 03:33:33 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+
+    "github.com/bwmarrin/discordgo"
+)
+
+var (
+    Token string
+)
+
+func main()  {
+
+    Token := os.Getenv("TOKEN")
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil {
+        fmt.Println("error creating Discord session,", err)
+        return
+    }
+
+    // Register the messageCreate func as a callback for MessageCreate events.
+    dg.AddHandler(messageCreate)
+
+    // Open a websocket connection to Discord and begin listening.
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("error opening connection,", err)
+        return
+    }
+
+    // Wait here until CTRL-C or other term signal is received.
+    fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Cleanly close down the Discord session.
+    dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+    if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+    fmt.Println(m.Content)
+}
